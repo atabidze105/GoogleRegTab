5bd5b04e5d4e7a3d9e677cae391b3bf72d1c936e
AlexVoronstov <AlexVoronstov>
2024-04-03 14:12:08 +0300

    Initial commit

---
+++ GoogleRegTab/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GoogleRegTab
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
